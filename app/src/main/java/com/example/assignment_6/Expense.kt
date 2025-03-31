a9defd41ef6c1fad8cb170f672b697d219df1298
Arshdeep Singh <arshdeep59645@gmail.com>
2025-03-31 15:01:41 -0400

    #1: pulled assignment-10 branch from assignment-9, added new fields in expense.kt for currency and convertedCost.

--- app/src/main/java/com/example/assignment_6/Expense.kt
+++ app/src/main/java/com/example/assignment_6/Expense.kt
@@ -3,5 +3,7 @@ package com.example.assignment_6
 data class Expense(
     val name: String,
     val amount: String,
+    val currency: String = "CAD",
+    val convertedCost: Double = 0.0,
     val date: String
 )
