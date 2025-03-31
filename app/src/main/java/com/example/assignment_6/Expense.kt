9253dd0118e89e0a1a437a2a8301c29a61902608
Arshdeep Singh <arshdeep59645@gmail.com>
2025-03-31 16:46:14 -0400

    #5: Modified xml files for code implimentation.

--- app/src/main/java/com/example/assignment_6/Expense.kt
+++ app/src/main/java/com/example/assignment_6/Expense.kt
@@ -3,7 +3,7 @@ package com.example.assignment_6
 data class Expense(
     val name: String,
     val amount: String,
+    val date: String,
     val currency: String = "CAD",
-    val convertedCost: Double = 0.0,
-    val date: String
+    val convertedCost: Double = 0.0
 )
